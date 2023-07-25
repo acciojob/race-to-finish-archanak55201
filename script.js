d69e59ea9e2bd5583e95f5d4b5ca393255147470
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-26 00:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let prom1= new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(20);
+	},2000)
+
+let prom2= new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(30);
+	},3000)
+})
+	let prom3= new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(60);
+	},6000)
+})
+	let prom4= new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(80);
+	},5000)
+})
+	let prom5= new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve(40);
+	},4000)
+})
+promises=[prom1,prom2,prom3,prom4,prom5];
+let x=Promise.any(promises);
+	document.getElementById("output").innerText=x;
+	
